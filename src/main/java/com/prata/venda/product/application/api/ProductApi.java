52fe47068d34e2ee859f7db617a7e887b7f447e8
matheusprata <matheusprata.ti@gmail.com>
2023-11-04 00:45:38 -0300

    adicionado tag para melhorar documentação do swagger

--- src/main/java/com/prata/venda/product/application/api/ProductApi.java
+++ src/main/java/com/prata/venda/product/application/api/ProductApi.java
@@ -1,19 +1,19 @@
 package com.prata.venda.product.application.api;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.validation.Valid;
 
-//Tag(name = "Product", description = "Product APIs")
+@Tag(name = "Product", description = "Product APIs")
 @RequestMapping("/v1/product")
 public interface ProductApi {
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(code = HttpStatus.CREATED)
     ProductIdResponse newProduct(@Valid @RequestBody ProductRequest productRequest);
 }
